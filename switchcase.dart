1c4f9f156b4e4fe2896692bff0d8cfe8cf64627c
Goivnd Kumar Yadav <102906652+Robertgovind@users.noreply.github.com>
2023-01-10 17:33:45 +0545

    Add files via upload

---
+++ switchcase.dart
@@ -0,0 +1,18 @@
+void main()
+{
+var find="fetching";
+switch(find){
+  case "fetching":
+   print("The instruction is being fetched.");
+   break;
+  case "decoding":
+   print("The instruction is being decoded.");
+   break;
+  case  "execute":
+   print("The instruction is being executed.");
+   break;
+  default:
+   print("Invalid");
+};
+
+}
